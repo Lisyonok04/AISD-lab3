44ad80920316e1840e01b1bd10bab695edeb8bce
Lisyonok04 <125237397+Lisyonok04@users.noreply.github.com>
2023-12-27 00:17:04 +0400

    Add files via upload

--- include.h
+++ include.h
@@ -12,3 +12,15 @@ namespace sorting {
             stream << mas[i] << " ";
         return stream;
     }
+
+    struct Stats {
+        size_t comparison_count = 0;
+        size_t copy_count = 0;
+
+        Stats& operator+=(const Stats& stats) {
+            comparison_count += stats.comparison_count;
+            copy_count += stats.copy_count;
+            return *this;
+        }
+    };
+
