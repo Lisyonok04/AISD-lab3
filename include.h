569d535175cc7895e2fd385eb9d2f4850c6c0bfe
Lisyonok04 <125237397+Lisyonok04@users.noreply.github.com>
2023-12-27 00:11:30 +0400

    Add files via upload

--- include.h
+++ include.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+namespace sorting {
+
+    template<typename T>
+    ostream& operator<<(std::ostream& stream, const vector<T>& mas) {
+        for (int i = 0; i < mas.size(); i++)
+            stream << mas[i] << " ";
+        return stream;
+    }
